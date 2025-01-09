07f989efbcdec7661c8e5a48f978bdfc087f5c4a
Ivan <ivan0vasutinskiy@gmail.com>
2025-01-09 18:54:51 +0300

    update prime.py

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -6,12 +6,14 @@ RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def start_game():
     num = randint(1, 100)
     total = 0
+
     for i in range(1, num + 1):
         if num % i == 0:
             total += 1
+        
 
     question = f'{num}' 
-    if total <= 2:
+    if total == 2:
         answaer = 'yes'
     else:
         answaer = 'no'
